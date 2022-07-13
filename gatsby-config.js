007bdd4023a12a016bbe71a6a51c3e7807637f72
Achiya Elyasaf <10044875+eggsterino@users.noreply.github.com>
2022-07-13 01:07:22 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,13 +21,6 @@ module.exports = {
         path: `${__dirname}/src/images`
       }
     },
-    {
-      resolve: "gatsby-source-contentful",
-      options: {
-        spaceId,
-        accessToken
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-transformer-remark`,
